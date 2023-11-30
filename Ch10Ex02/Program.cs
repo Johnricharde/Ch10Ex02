cf049a365dfc5b7f275dbd47b46dd4abd306ec10
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-30 08:45:25 +0100

    Add project files.

---
+++ Ch10Ex02/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ch10Ex02
+{
+    public class ClassA
+    {
+        private int state = -1;
+        public int State => state;
+        public class ClassB
+        {
+            public void SetPrivateState(ClassA target, int newState)
+            {
+                target.state = newState;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myObject = new ClassA();
+            Console.WriteLine($"myObject.State = {myObject.State}");
+            var myOtherObject = new ClassA.ClassB();
+            myOtherObject.SetPrivateState(myObject, 999);
+            Console.WriteLine($"myObject.State = {myObject.State}");
+            Console.ReadKey();
+        }
+    }
+}
